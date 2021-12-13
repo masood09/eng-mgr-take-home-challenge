c18933af91374a6f32f2bcb479bf0ad28ec70339
Masood Ahmed <masood.ahmed09@gmail.com>
2021-12-12 23:28:57 -0500

    Connecting to the database

--- flask/api/index.py
+++ flask/api/index.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from os import environ
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+from api.database import initialize_db
+
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI')
+
+initialize_db(app)
 
 @app.route('/')
 def hello_world():
